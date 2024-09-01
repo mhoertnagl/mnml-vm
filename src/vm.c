657f7011e560f2d8773b1c30b15712e9479525c3
Mathias Hörtnagl <mathias.hoertnagl@gmail.com>
2024-09-01 11:17:21 +0200

    function calls simple

--- src/vm.c
+++ src/vm.c
@@ -12,8 +12,7 @@
   (u64)vm.ip[6] <<  8 | \
   (u64)vm.ip[7]         \
 )
-#define NEXT_ARG()       \
-(                        \
+#define NEXT_ARG() (     \
   vm.ip += 8,            \
   (u64)vm.ip[-8] << 56 | \
   (u64)vm.ip[-7] << 48 | \
@@ -27,8 +26,9 @@
 #define PUSHD(val) (*vm.dp = (val), vm.dp++)
 #define POPD()     (*(--vm.dp))
 #define PEEKD()    (*(vm.dp - 1))
-#define PUSHR(val) (*vm.rp = (val), vm.rp++)
-#define POPR()     (*(--vm.rp))
+#define PUSHC(val) (*vm.cp = (val), vm.cp++)
+// #define POPC()     (*(--vm.cp))
+#define PEEKC(off) (*(vm.fp + (off)))
 #define BINOPI(op) u64 r = POPD(); PEEKD() op##= r
 #define BINOP(op)  u64 r = POPD(); PEEKD() = PEEKD() op r
 
@@ -37,29 +37,63 @@ Vm vm;
 VmResult vm_run(u8 *code) {
   vm.ip = code;
   vm.dp = vm.dat;
-  vm.rp = vm.ret; 
+  vm.cp = vm.cal;
+  vm.fp = vm.cal;
   for (;;) {
     // switch (NEXT_OP() & 0x1f) {
     switch (NEXT_OP()) {
-      case OP_PSH: { PUSHD(NEXT_ARG()); break; } 
-      case OP_DUP: { PUSHD(PEEKD()); break; }
-      case OP_LDW: { PEEKD() = vm.mem[PEEKD()]; break; }
-      case OP_STW: { u64 v = POPD(); u64 a = POPD(); vm.mem[a] = v; break; }
-      case OP_ADD: { BINOPI(+); break; }
-      case OP_SUB: { BINOPI(-); break; } 
-      case OP_MUL: { BINOPI(*); break; }  
-      case OP_DIV: { BINOPI(/); break; }
-      case OP_EQU: { BINOP(==); break; }
-      case OP_NEQ: { BINOP(!=); break; }
-      case OP_SGT: { BINOP(>);  break; }
-      case OP_SGE: { BINOP(>=); break; }
-      case OP_SLT: { BINOP(<);  break; }
-      case OP_SLE: { BINOP(<=); break; }
-      case OP_JMP: { vm.ip = code + PEEK_ARG(); break; }
-      case OP_BRA: { if (POPD()) vm.ip = code + PEEK_ARG(); break; }
-      case OP_JAL: { PUSHR(vm.ip + 1); vm.ip = code + PEEK_ARG(); break; }
-      case OP_RET: { vm.ip = POPR(); break; }
-      case OP_HLT: return VM_SUCCESS;
+      case OP_PSH:  { PUSHD(NEXT_ARG()); break; } 
+      case OP_DUP:  { PUSHD(PEEKD()); break; }
+      // case OP_LDW: { PEEKD() = vm.mem[PEEKD()]; break; }
+      // case OP_STW: { u64 v = POPD(); u64 a = POPD(); vm.mem[a] = v; break; }
+      case OP_ADD:  { BINOPI(+); break; }
+      case OP_SUB:  { BINOPI(-); break; } 
+      case OP_MUL:  { BINOPI(*); break; }  
+      case OP_DIV:  { BINOPI(/); break; }
+      case OP_EQ:   { BINOP(==); break; }
+      case OP_NE:   { BINOP(!=); break; }
+      case OP_GT:   { BINOP(>);  break; }
+      case OP_GE:   { BINOP(>=); break; }
+      case OP_LT:   { BINOP(<);  break; }
+      case OP_LE:   { BINOP(<=); break; }
+      case OP_JMP:  { vm.ip = code + PEEK_ARG(); break; }
+      case OP_JNZ:  { 
+        u64 offset = NEXT_ARG(); 
+        if (!POPD()) vm.ip = code + offset; 
+        break; 
+      }
+      case OP_CALL: { 
+        vm.cp[0] = (u64)vm.fp;
+        vm.cp[1] = (u64)(vm.ip + 8 + 1);
+        vm.fp    = vm.cp;
+        vm.cp   += 2;
+        vm.ip    = code + PEEK_ARG();
+        break;
+      }
+      // case OP_CALL: {
+      //   vm.cp[0] = (u64)vm.fp;
+      //   vm.cp[1] = (u64)(vm.ip + 8 + 1);
+      //   vm.ip    = code + NEXT_ARG();
+      //   u64 argc = NEXT_ARG();
+      //   for (u64 i = 0; i < argc; i++) {
+      //     vm.cp[i + 2] = vm.dp[i - argc];
+      //   }
+      //   vm.fp = vm.cp;
+      //   vm.cp += argc + 2;
+      //   vm.dp -= argc;
+      //   break; 
+      // }
+      case OP_RET:  { 
+        vm.cp = vm.fp;
+        vm.fp = (u64 *)(*vm.cp);
+        vm.ip = (u8 *)(*(vm.cp + 1)); 
+        break; 
+      }
+      case OP_PSHV: { PUSHC(POPD()); break; }
+      case OP_LDV:  { PUSHD(PEEKC(NEXT_ARG())); break; }
+      // case OP_LDV:  { PEEKD() = PEEKC(PEEKD()); break; }
+      case OP_STV:  { PEEKC(NEXT_ARG()) = POPD(); break; }
+      case OP_HLT:  return VM_SUCCESS;
       default: return VM_ERROR_UNKNOWN_OPCODE;
       // case 18 ... 31: return VM_ERROR_UNKNOWN_OPCODE;
     }
--- src/vm.h
+++ src/vm.h
@@ -3,12 +3,12 @@
 #include <inttypes.h>
 #include <stdint.h>
 
-typedef uint8_t u8;
+typedef uint8_t  u8;
 typedef uint64_t u64;
 
-#define MEMORY_SIZE       65536
-#define DATA_STACK_SIZE     512
-#define RETURN_STACK_SIZE   256
+// #define MEMORY_SIZE     65536
+#define DATA_STACK_SIZE 1024
+#define CALL_STACK_SIZE 1024
 
 typedef enum {
   VM_SUCCESS,
@@ -20,29 +20,31 @@ typedef enum {
   OP_DUP,
   // OP_LDI,
   // OP_STI,
-  OP_LDW,
-  OP_STW,
+  // OP_LDW,
+  // OP_STW,
   OP_ADD,
   OP_SUB,
   OP_MUL,
   OP_DIV,
   // OP_AND,
-  // OP_OOR,
+  // OP_OR,
   // OP_NOT,
   // OP_SLL,
   // OP_SRL,
-  OP_EQU,
-  OP_NEQ,
-  OP_SGT,
-  OP_SGE,
-  OP_SLT,
-  OP_SLE,
+  OP_EQ,
+  OP_NE,
+  OP_GT,
+  OP_GE,
+  OP_LT,
+  OP_LE,
   OP_JMP,
-  OP_BRA,
-  // OP_BEQ,
-  // OP_BNE,
-  OP_JAL,
+  OP_JNZ,
+  // OP_JEQ,
+  OP_CALL,
   OP_RET,
+  OP_PSHV,
+  OP_LDV,
+  OP_STV,
   OP_HLT  // Halt the VM.
 } OpCode;
 
@@ -52,11 +54,12 @@ typedef struct {
   // Fixed-size data stack.
   u64 dat[DATA_STACK_SIZE];
   u64 *dp;
-  // Fixed-size return stack.
-  u8 *ret[RETURN_STACK_SIZE];
-  u8 **rp;
+  // Fixed-size call stack.
+  u64 cal[CALL_STACK_SIZE];
+  u64 *cp;
+  u64 *fp;
   // Fixed-size memory.
-  u64 mem[MEMORY_SIZE];
+  // u64 mem[MEMORY_SIZE];
 } Vm;
 
 extern Vm vm;
